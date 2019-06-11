c54aa82f27a74ef20487e680a29ee37d2e240c48
robert velasco <robertvelasco99@gmail.com>
2019-06-11 01:27:44 -0500

    Se modifico JS tbl , no changes

--- js/tbl.js
+++ js/tbl.js
@@ -1,14 +1,15 @@
-document.addEventListener('mousemove',createTbl());
-
 var i = 0;
-function createTbl(){
+
+document.getElementById('butt').addEventListener('click', () => {
     var table = document.getElementById("myTable");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
     i++;
     cell1.innerHTML = i;
-    cell2.innerHTML = "NEW CELL"+i;
+    cell2.innerHTML = "Roberto A. Velasco Munguía";
     cell3.innerHTML = "Waaaaa";
-}
+    cell4.innerHTML = "deomeof";
+});
